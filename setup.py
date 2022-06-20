0371d57715e1cc53975f523332a22b5c326d6ecb
Felipe Menino <efelipecarlos@gmail.com>
2022-06-20 17:01:33 -0300

    docs: fixing docs installation

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ tests_require = [
     'invenio-app>=1.3.1,<2.0.0',
     'pytest-invenio>=1.4.2,<2.0.0',
     'pytest-mock>=1.6.0',
+    'Sphinx>=4.2.0'
 ]
 
 # Should follow inveniosoftware/invenio versions
